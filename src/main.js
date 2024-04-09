5733ae734694386c719472e1c8df73b1b4abd0ea
Naresh <100291309+naresh1219990@users.noreply.github.com>
2024-04-09 22:41:59 +0530

    add final

--- src/main.js
+++ src/main.js
@@ -68,11 +68,85 @@ const toggleSearch = () => {
 
 
 
+  
+
+
   // window.onbeforeunload = () => {
   //   for(const form of document.getElementsByTagName('form')) {
   //     form.reset();
   //   }
   // }
+
+
+  // searchbar
+
+  document.getElementById('searchInput').addEventListener('input', function(event) {
+    const searchQuery = event.target.value.toLowerCase();
+    const suggestionsContainer = document.getElementById('searchSuggestions');
+    suggestionsContainer.innerHTML = ''; // Clear previous suggestions
+    if (searchQuery.length >= 3) {
+      const suggestions = [
+        { name: 'Home', link: './index.html#Home' },
+        { name: 'About Us', link: './index.html#about' },
+        { name: 'Our Products', link: './OurProduct.html' },
+        { name: 'Services', link: './Service.html' },
+        { name: 'Contact', link: './index.html#contact' },
+      ];
+  
+      const filteredSuggestions = suggestions.filter(suggestion =>
+        suggestion.name.toLowerCase().includes(searchQuery)
+      );
+  
+      filteredSuggestions.forEach(suggestion => {
+        const suggestionElement = document.createElement('div');
+        suggestionElement.textContent = suggestion.name;
+        suggestionElement.classList.add('p-2', 'cursor-pointer');
+        suggestionElement.addEventListener('click', () => {
+          window.location.href = suggestion.link;
+        });
+        suggestionsContainer.appendChild(suggestionElement);
+      });
+  
+      suggestionsContainer.style.display = filteredSuggestions.length > 0 ? 'block' : 'none';
+    } else {
+      suggestionsContainer.style.display = 'none';
+    }
+  });
+  
+  // Handling navigation to the section on "Enter"
+  document.getElementById('searchInput').addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+      const inputVal = this.value.toLowerCase();
+      const suggestionLinks = {
+        'home': './index.html#Home',
+        'about us': './index.html#about',
+        'our products': './OurProduct.html',
+        'services': './Service.html',
+        'contact': './index.html#contact',
+      };
+  
+      const matchedLink = Object.entries(suggestionLinks).find(([key]) => key.includes(inputVal));
+      if (matchedLink) {
+        window.location.href = matchedLink[1];
+      }
+    }
+  });
+  
+  
+  // Function to navigate to the section or external page
+  function navigateToSection(sectionId) {
+    if (sectionId.includes('.html')) {
+      // If the ID indicates an external page, change location
+      window.location.href = sectionId;
+    } else {
+      // For internal sections, scroll into view
+      const sectionElement = document.getElementById(sectionId);
+      if (sectionElement) {
+        sectionElement.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }
+
 
 
               
