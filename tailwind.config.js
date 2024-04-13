55a03b47625f9121b579511e307cfe319eaf04d3
Naresh <100291309+naresh1219990@users.noreply.github.com>
2024-04-13 20:32:56 +0530

    final produ

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,28 +3,43 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      colors:{
-        primary:"#363636",
-        secondry:'#0077BE',
-        line:"#0094DA",
-        menu:"#272727",
-        abou:"#494949",
-        white:"#FFFFFF",
-        drop:"#F9F9F9"
+      colors: {
+        primary: "#363636",
+        secondry: "#0077BE",
+        line: "#0094DA",
+        menu: "#272727",
+        abou: "#494949",
+        white: "#FFFFFF",
+        drop: "#F9F9F9",
       },
-      fontFamily:{
-        poppin:[ "Poppins"],
+      fontFamily: {
+        poppin: ["Poppins"],
         inter: ["Inter"],
-        pompiere:["Pompiere"]
+        pompiere: ["Pompiere"],
       },
-      height:{
-        max:'542px',
-        prolg:'367px',
-        promd:'335px',
-        navb:'110px'
-      }
+      height: {
+        max: "542px",
+        prolg: "367px",
+        promd: "335px",
+        navb: "110px",
+        heromd: "300px",
+        herolg: "400px",
+        heroxl: "450px",
+        ourxl: "600px",
+        proimg: "277px",
+      },
+      width: {
+        wimg: "277px",
+      },
+
+      clipPath: {
+        "b-right-half": "inset(0 0 50% 50%)",
+      },
+      borderWidth: {
+        borw: "10px",
+      },
     },
   },
   plugins: [],
-}
+};
 
